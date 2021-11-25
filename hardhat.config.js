b4eccdcdef2586c4c752e8c36a3a8691b596ed3a
gg447062 <gg447062@gmail.com>
2021-11-25 16:28:20 -0600

    adds rinkeby testnet data to hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,14 @@
 require('@nomiclabs/hardhat-ethers');
+require('dotenv').config();
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: '0.8.2',
+  networks: {
+    rinkeby: {
+      url: process.env.MORALIS_RINKEBY_URL,
+      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
+    },
+  },
 };
