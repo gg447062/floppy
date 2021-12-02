85561db0bc86ee86a22f00711162fb9d7dd71aeb
gg447062 <gg447062@gmail.com>
2021-12-01 23:31:36 -0600

    adds COOP and COER headers

--- server/index.js
+++ server/index.js
@@ -11,13 +11,20 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// set COOP and COER headers so ffmpeg.wasm works
+app.use((req, res, next) => {
+  res.header('Cross-Origin-Embedder-Policy', 'require-corp');
+  res.header('Cross-Origin-Opener-Policy', 'same-origin');
+  next();
+});
+
 // static middleware
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '..', '/public')));
 
 app.use('/api', require('./api'));
 
 app.use('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, '..', '/public/index.html'));
 });
 
 app.use((err, req, res, next) => {
