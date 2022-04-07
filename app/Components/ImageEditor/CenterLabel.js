7632586c844efae629526fbbc3728a0206999eb4
Gabriel Gutierrez <gg447062@gmail.com>
2022-04-07 13:12:25 -0500

    begins building center label color control

--- app/Components/ImageEditor/CenterLabel.js
+++ app/Components/ImageEditor/CenterLabel.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const CenterLabel = ({ chooseStamp }) => {
+  const cl = useSelector((state) => state.editor.cl);
+  const filter = useSelector((state) => state.editor.filter);
+
+  const drawCenterLabel = (e) => {
+    cl.ctx.clearRect(0, 0, cl.canvas.width, cl.canvas.height);
+    cl.ctx.filter = filter;
+    console.log(e.target);
+    cl.ctx.drawImage(e.target, 0, 0, 522, 522);
+  };
+
   return (
     <div className="container stamps">
+      <img
+        id={`cl_image`}
+        className="stamp cl"
+        src={`assets/RECORD_CENTERLABEL/record.png`}
+        onClick={drawCenterLabel}
+      />
       {[...Array(13)].map((_, i) => {
         return (
           <img
