cca71a49feac2a0cabeee782c29e5523e7cb31a5
Gabriel Gutierrez <gg447062@gmail.com>
2022-06-28 09:40:53 -0500

    updates react version

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import App from './Components/App';
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { MoralisProvider } from 'react-moralis';
 import store from './store';
 import { Provider } from 'react-redux';
@@ -8,11 +9,13 @@ import { Provider } from 'react-redux';
 const appId = process.env.TEST_APP_ID;
 const serverURL = process.env.TEST_SERVER_URL;
 
-ReactDOM.render(
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(
   <MoralisProvider appId={appId} serverUrl={serverURL}>
     <Provider store={store}>
       <App />
     </Provider>
-  </MoralisProvider>,
-  document.getElementById('app')
+  </MoralisProvider>
 );
