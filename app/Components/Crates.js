20fec8ca29ba31ff255cf23422a79bfab0d5279d
Gabriel Gutierrez <gg447062@gmail.com>
2022-07-09 16:35:14 -0500

    adds audio player and some basic style

--- app/Components/Crates.js
+++ app/Components/Crates.js
@@ -2,6 +2,147 @@ import React, { useEffect, useState } from 'react';
 import { useMoralisQuery } from 'react-moralis';
 import axios from 'axios';
 import MainCanvas from './RecordViewer';
+import { moralisGateway } from '../utils';
+
+function ControlsTop({ dubplate, previous, next }) {
+  const player = document.getElementById('player');
+  const play = () => {
+    player.play();
+  };
+  const pause = () => {
+    player.pause();
+  };
+  return (
+    <div
+      style={{
+        disply: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        // maxHeight: '30%',
+        backgroundColor: 'pink',
+        padding: '1em',
+        border: '1px solid black',
+        boxSizing: 'border-box',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexGrow: '1',
+          columnGap: '1em',
+        }}
+      >
+        <div
+          style={{
+            flexGrow: '1',
+            height: '2em',
+            backgroundColor: 'black',
+          }}
+        />
+        <h1 style={{ color: 'black' }}>Floppy</h1>
+        <div
+          style={{
+            flexGrow: '1',
+            height: '2em',
+            backgroundColor: 'black',
+          }}
+        />
+      </div>
+      <div
+        style={{
+          border: '1px solid blue',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            columnGap: '3em',
+            padding: '1em',
+            width: '100%',
+            maxWidth: '100%',
+          }}
+        >
+          <div style={{ backgroundColor: 'black', fontSize: '2em' }}>4:20</div>
+          <div>
+            <div
+              style={{
+                display: 'flex',
+                columnGap: '1em',
+                backgroundColor: 'black',
+                fontSize: '2em',
+              }}
+            >
+              <div>{dubplate.artist}</div>
+              <div>{dubplate.track}</div>
+            </div>
+          </div>
+        </div>
+
+        <div
+          style={{
+            padding: '1em',
+          }}
+        >
+          <button onClick={previous}>prev</button>
+          <button onClick={play}>play</button>
+          <button onClick={pause}>pause</button>
+          <button onClick={next}>next</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function ControlsBottom({ dubplates }) {
+  return (
+    <div
+      style={{
+        backgroundColor: 'pink',
+        flexGrow: '1',
+        width: '100%',
+        padding: '1em',
+        boxSizing: 'border-box',
+      }}
+    >
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          flexGrow: '1',
+          columnGap: '1em',
+        }}
+      >
+        <div
+          style={{
+            flexGrow: '1',
+            height: '2em',
+            backgroundColor: 'black',
+          }}
+        />
+        <h1 style={{ color: 'black' }}>Playlist</h1>
+        <div
+          style={{
+            flexGrow: '1',
+            height: '2em',
+            backgroundColor: 'black',
+          }}
+        />
+      </div>
+      <div style={{ backgroundColor: 'black', flexGrow: '1', padding: '1em' }}>
+        {dubplates.map((dubplate, i) => {
+          return (
+            <p key={i}>
+              {dubplate.artist} - {dubplate.track}
+            </p>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
 
 export default function Crates() {
   const { data, error, isLoading } = useMoralisQuery('Dubplate');
@@ -43,52 +184,53 @@ export default function Crates() {
     }
   };
 
+  console.log(dubplates[index]);
+
   return (
     <div
       id="crates"
       style={{
         height: '100vh',
         width: '100vw',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
       }}
     >
-      <div
-        onClick={next}
-        style={{ position: 'absolute', right: '1em', top: '4em', zIndex: 1000 }}
-      >
-        Next
-      </div>
-      <div
-        onClick={previous}
-        style={{ position: 'absolute', left: '1em', top: '4em', zIndex: 1000 }}
-      >
-        Previous
-      </div>
       {error && <div>error...</div>}
       {isLoading && <div>loading...</div>}
       {dubplates[index] && (
         <div
-          style={{ height: '80vh', width: '40vw', border: '1px solid green' }}
+          style={{
+            height: '100vh',
+            width: '100vw',
+            display: 'flex',
+          }}
         >
+          <div style={{ width: '50%' }}>
+            {backURL && <MainCanvas front={frontURL} back={backURL} />}
+          </div>
           <div
             style={{
-              width: '100%',
+              height: '100vh',
+              width: '50%',
               display: 'flex',
-              justifyContent: 'center',
-              gap: '2em',
-              fontSize: '2em',
-              position: 'absolute',
-              top: '0',
-              left: '0',
+              flexDirection: 'column',
+              alignItems: 'center',
+              border: '1px solid green',
+              backgroundColor: 'green',
+              // padding: '2em',
+              boxSizing: 'border-box',
             }}
           >
-            <div>{dubplates[index]?.artist}</div>
-            <div>{dubplates[index]?.track}</div>
+            <ControlsTop
+              dubplate={dubplates[index]}
+              next={next}
+              previous={previous}
+            />
+            <ControlsBottom dubplates={dubplates} />
           </div>
-          {backURL && <MainCanvas front={frontURL} back={backURL} />}
+          <audio
+            id="player"
+            src={`${moralisGateway}/${dubplates[index].metadata.audio}`}
+          ></audio>
         </div>
       )}
     </div>
