a30419126e9d1709408bf9deb71c6dd7a4468b21
Gabriel Gutierrez <gg447062@gmail.com>
2022-09-14 18:05:33 -0500

    adds grid view for crates

--- app/Components/Gallery.js
+++ app/Components/Gallery.js
@@ -1,37 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useMoralisQuery } from 'react-moralis';
+import { moralisGateway } from '../utils';
 
-const samples = [
-  { image: '💾', name: 'sample1' },
-  { image: '💾', name: 'sample2' },
-  { image: '💾', name: 'sample3' },
-  { image: '💾', name: 'sample4' },
-  { image: '💾', name: 'sample5' },
-  { image: '💾', name: 'sample6' },
-];
-
-const Gallery = (props) => {
+const SingleView = ({ dubplate, setViewSingle }) => {
   return (
-    <div id="gallery" className="modal container">
-      <button
-        onClick={() => {
-          props.showGallery(false);
-        }}
-      >
-        X
-      </button>
-      <h1>Floppy Gallery</h1>
-      <table>
-        <tbody>
-          {samples.map((sample, index) => {
-            return (
-              <tr key={index}>
-                <td>{sample.image}</td>
-                <td>{sample.name}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <div className="dubplate-grid-view__single-wrapper">
+      <div className="dubplate-grid-view__single">
+        <div
+          onClick={() => {
+            setViewSingle(false);
+          }}
+        >
+          X
+        </div>
+        <img src={`${moralisGateway}/${dubplate.metadata.front}`} />
+        <p>
+          {dubplate.artist} - {dubplate.track}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const Gallery = () => {
+  const { data, error, isLoading } = useMoralisQuery('Dubplate');
+  const [dubplates, setDubplates] = useState([]);
+  const [single, setSingle] = useState(null);
+  const [viewSingle, setViewSingle] = useState(false);
+
+  const openModal = (e) => {
+    console.log(e.target.id);
+    setSingle(dubplates[e.target.id]);
+    setViewSingle(true);
+  };
+
+  useEffect(() => {
+    if (data) {
+      const _dubplates = data.map((el) => el.attributes);
+      setDubplates(_dubplates);
+    }
+  }, [data]);
+
+  console.log(single, viewSingle);
+  return (
+    <div className="grid-view ff-3">
+      {dubplates &&
+        dubplates.map((dubplate, i) => {
+          return (
+            <div
+              className="dubplate-grid-view"
+              id={i}
+              key={i}
+              onClick={openModal}
+            >
+              <img
+                src={`${moralisGateway}/${dubplate.metadata.front}`}
+                id={i}
+              />
+              <p id={i}>
+                {dubplate.artist} - {dubplate.track}
+              </p>
+            </div>
+          );
+        })}
+      {viewSingle && (
+        <SingleView dubplate={single} setViewSingle={setViewSingle} />
+      )}
     </div>
   );
 };
