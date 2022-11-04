35961c89220a8f1e9e2da67dbbe395bb170cf03d
Gabriel Gutierrez <gg447062@gmail.com>
2022-11-04 15:52:55 -0600

    continues styling 3d player

--- app/Components/Crates/Player3D.js
+++ app/Components/Crates/Player3D.js
@@ -1,53 +1,125 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useMoralisQuery } from 'react-moralis';
 import axios from 'axios';
 import MainCanvas from './RecordViewer';
 import { moralisGateway } from '../../utils';
 
-function ControlsTop({ dubplate, previous, next }) {
-  const player = document.getElementById('player');
-  const play = () => {
-    player.play();
+function AudioPlayer({ dubplate, previous, next }) {
+  const audioRef = useRef();
+  const timelineRef = useRef();
+  const [src, setSrc] = useState(
+    'assets/crates_ui_assets/cratesplaybutton_sizedforpopup.png'
+  );
+
+  const play = (e) => {
+    if (
+      audioRef.current.paused ||
+      audioRef.current.cureetTime == 0 ||
+      audioRef.current.currentTime == audioRef.current.duration
+    ) {
+      audioRef.current.play();
+      setSrc('assets/listener_3d_assets/Pause_button.png');
+    } else {
+      audioRef.current.pause();
+      setSrc('assets/crates_ui_assets/cratesplaybutton_sizedforpopup.png');
+    }
   };
-  const pause = () => {
-    player.pause();
+
+  const scrub = (e) => {
+    audioRef.current.currentTime =
+      (e.target.value * audioRef.current.duration) / 100;
   };
+
+  useEffect(() => {
+    const update = () => {
+      const percentagePosition = (
+        (100 * audioRef.current.currentTime) /
+        audioRef.current.duration
+      ).toFixed(1);
+      timelineRef.current.style.backgroundSize = `${percentagePosition}% 100%`;
+      timelineRef.current.value = percentagePosition;
+    };
+
+    if (audioRef.current) {
+      audioRef.current.ontimeupdate = update;
+    }
+  });
+
   return (
     <div className="controls-wrapper ">
       <div className="controls-inner-wrapper">
         <div className="button-wrapper">
-          <button className="" onClick={previous}>
-            prev
-          </button>
-          <button className="" onClick={play}>
-            play
-          </button>
-          <button className="" onClick={pause}>
-            pause
-          </button>
-          <button className="" onClick={next}>
-            next
-          </button>
+          <img
+            src="assets/listener_3d_assets/previous_track_button.png"
+            onClick={previous}
+          />
+          <img src={src} onClick={play} />
+          <img
+            src="assets/listener_3d_assets/next_track_button.png"
+            onClick={next}
+          />
         </div>
-        <div className="track-info ">
-          <div>{dubplate.artist}</div>
-          <div>-</div>
-          <div>{dubplate.track}</div>
+        <div className="player-3d-middle-row">
+          <div className={`player-3d-timeline-wrapper`}>
+            <input
+              type="range"
+              className={`timeline-3d`}
+              max={100}
+              step={0.1}
+              defaultValue={0}
+              onChange={scrub}
+              ref={timelineRef}
+            ></input>
+            <img
+              className={`player-3d-timeline-bg`}
+              src="assets/listener_3d_assets/playlist_track_scoller_background.png"
+            ></img>
+            <img
+              className={`player-3d-timeline-bg-upper`}
+              src="assets/listener_3d_assets/track_scroll_black.png"
+            ></img>
+          </div>
+          <img
+            className="playlist-button-default"
+            src="assets/listener_3d_assets/playlist_button_default.png"
+          ></img>
+        </div>
+        <div className="track-info">
+          <div>
+            <p>{dubplate.artist}</p>
+            <p>-</p>
+            <p>{dubplate.track}</p>
+          </div>
+          <img src="assets/listener_3d_assets/buy_button_default.png" />
         </div>
       </div>
+      <audio
+        id="player"
+        ref={audioRef}
+        src={`${moralisGateway}/${dubplate.metadata.audio}`}
+      ></audio>
     </div>
   );
 }
 
-function Playlist({ dubplates }) {
+function Playlist({ dubplates, current }) {
   return (
-    <div className="controls-bottom-wrapper ">
+    <div className="playlist-outer-wrapper ">
       <div className="playlist-wrapper ">
         {dubplates.map((dubplate, i) => {
           return (
-            <p key={i}>
-              {dubplate.artist} ------ {dubplate.track}
-            </p>
+            <div
+              className={`playlist-item${current === i ? '-selected' : ''}`}
+              key={i}
+            >
+              <p>
+                {dubplate.artist} ------ {dubplate.track}
+              </p>
+              <div>
+                <p>2:03</p>
+                <img src="assets/listener_3d_assets/buy_button_default.png" />
+              </div>
+            </div>
           );
         })}
       </div>
@@ -105,17 +177,13 @@ export default function Player3D() {
             {backURL && <MainCanvas front={frontURL} back={backURL} />}
           </div>
           <div className="crates-playlist-wrapper">
-            <Playlist dubplates={dubplates} />
-            <ControlsTop
+            <Playlist dubplates={dubplates} current={index} />
+            <AudioPlayer
               dubplate={dubplates[index]}
               next={next}
               previous={previous}
             />
           </div>
-          <audio
-            id="player"
-            src={`${moralisGateway}/${dubplates[index].metadata.audio}`}
-          ></audio>
         </div>
       )}
     </div>
