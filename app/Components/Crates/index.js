167568d6d072e27c7101d0c51e00bcd4a5100030
Gabriel Gutierrez <gg447062@gmail.com>
2022-11-14 18:56:53 -0600

    adds button to toggle 3d view in crates

--- app/Components/Crates/index.js
+++ app/Components/Crates/index.js
@@ -3,12 +3,20 @@ import Grid from './Grid';
 import Player3D from './Player3D';
 
 export default function Crates() {
-  const [view, setView] = useState('3d');
+  const [grid, setGrid] = useState(false);
+  const view = grid ? '3D' : 'GRID';
+  const toggle = () => {
+    const _grid = !grid;
+    setGrid(_grid);
+  };
 
   return (
     <div>
-      {view == 'grid' && <Grid />}
-      {view == '3d' && <Player3D />}
+      <button className="toggle-switch" onClick={toggle}>
+        {view}
+      </button>
+      {grid && <Grid />}
+      {!grid && <Player3D />}
     </div>
   );
 }
