d292ef2d7904e7405674dea6e540a45de36e6e49
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2022-12-27 16:49:20 -0700

    updates downloadWAV function to retry fetch 5 times

--- app/lib/storage.js
+++ app/lib/storage.js
@@ -2,6 +2,7 @@ import { storage } from '../lib/firebase';
 import { ref, getDownloadURL } from 'firebase/storage';
 
 export function downloadWAV(path) {
+  let tries = 0
   console.log('downloading...');
   getDownloadURL(ref(storage, path))
     .then((url) => {
@@ -10,7 +11,12 @@ export function downloadWAV(path) {
       console.log('fetched successfully');
     })
     .catch((error) => {
-      console.log('error fetching')
-      console.log(error);
+      if (tries < 5) {
+        tries += 1
+        return downloadWAV(path)
+      } else {
+        console.log('error fetching')
+        console.log(error);
+      }
     });
 }
