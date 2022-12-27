c65e90fc1d5843bc1fcac74018ed1b26ca1f6c1c
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2022-12-27 16:48:01 -0700

    removes unused config data from firebase app

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -10,7 +10,6 @@ const app = initializeApp({
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 });
 
 export const db = getFirestore(app);
