b55acadf42fa5e1f18f91031ca668a76fbb902ff
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-02-27 14:02:01 -0600

    requires wallet connect to play

--- app/Components/SplashPage.js
+++ app/Components/SplashPage.js
@@ -1,27 +1,46 @@
 import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
-const baseURL = 'https://dg3mov3znt8u.cloudfront.net';
+// const baseURL = 'https://dg3mov3znt8u.cloudfront.net';
 
 export default function SplashPage({ introModalRef, player }) {
+  const isAuthenticated = useSelector((state) => state.user.authenticated);
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
+  const [showMessage, setShowMessage] = useState(false);
 
-  const goToCrates = () => {
-    navigate('/crates');
+  const hide = () => {
+    setShowMessage(false);
   };
 
   const load = () => {
+    if (!isAuthenticated) {
+      setShowMessage(true);
+      return;
+    }
     player.startInstance();
     setLoading(true);
   };
 
   return (
     <div id="intro-modal" ref={introModalRef}>
+      {showMessage && (
+        <div className="message-modal">
+          <div onClick={hide}>x</div>
+          <p>must connect wallet to play</p>
+        </div>
+      )}
       {!loading && (
         <div id="intro-button-wrapper">
           <button onClick={load}>Play</button>
-          <button onClick={goToCrates}>Crates</button>
+          <button
+            onClick={() => {
+              navigate('/crates');
+            }}
+          >
+            Crates
+          </button>
         </div>
       )}
       {loading && <div id="loading-message">Loading...</div>}
