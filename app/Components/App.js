39cfb446f3733a1fe1bb1bd7fe6566835ccd70dd
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-02-27 14:01:30 -0600

    removes audio test component

--- app/Components/App.js
+++ app/Components/App.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Game from './Game';
 import ImageEditor from './ImageEditor';
 import Upload from './Upload';
 import Crates from './Crates';
-import AudioTest from './AudioTest';
 
 const App = () => {
   return (
@@ -15,7 +14,6 @@ const App = () => {
           <Route path={'/crates'} element={<Crates />} />
           <Route path={'/editor'} element={<ImageEditor />} />
           <Route path={'/upload'} element={<Upload />} />
-          <Route path={'/audio'} element={<AudioTest />} />
         </Routes>
       </Router>
     </main>
