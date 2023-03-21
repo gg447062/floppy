b79b5365e573154a1613c392035972ddeaf95593
Gabriel Gutierrez <gabrielgutierrez@Gabriels-MacBook-Pro.local>
2023-03-21 15:16:48 -0600

    updates env names

--- app/lib/firebase.js
+++ app/lib/firebase.js
@@ -1,15 +1,15 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import {getAuth} from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import 'firebase/functions';
 import 'firebase/storage';
 
 const app = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 });
 
 export const db = getFirestore(app);
